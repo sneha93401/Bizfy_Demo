ba86c83f133f70709501b2f5cf40eb852f33307e
Sneha Jain <156401777+sneha93401@users.noreply.github.com>
2024-12-16 17:42:20 +0530

    Update Navbar.jsx

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-scroll"; // Import Link from react-scroll
-import "./Navbar.css";
+import "./NAvbar.css";
 
 const Navbar = () => {
   return (
